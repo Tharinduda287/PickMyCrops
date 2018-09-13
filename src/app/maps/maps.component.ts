9ad995e23b51d1299fe33b2fb9ffe5da44dc2222
chandanakgd <kgdilipchandana@gmail.com>
2018-09-13 11:37:32 +0530

    map done should be added to right api key to index.html page

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -15,11 +15,42 @@ draggable?: boolean;
 })
 export class MapsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() { 
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //     this.lat=position.coords.latitude;
+    //     this.lng=position.coords.longitude;
+    //   })
+    var origin1 = {lat: 51.373858, lng: 7.215982};
+    // var origin2 = 'Greenwich, England';
+    var destinationA = { lat: 51.723858, lng: 7.895982 };
+    var destinationB = {lat: 51.38693575008736, lng: 7.504503391776552};
+    
+    this.service.getDistanceMatrix(
+      {
+        origins: [origin1],
+        destinations: [destinationA,destinationB],
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false
+      }, callback);
+    
+    function callback(response, status) {
+      // See Parsing the Results for
+      // the basics of a callback function.  
+      var minVal=Math.min.apply(null,response.rows[0].elements.map(o => o.distance.value))
+      console.log(response.rows[0].elements.filter(o => o.distance.value==minVal))   
+    }
+  }
+  lat:number=7.250937;
+  lng:number=80.592800;
+  curLat:number;
+  curLng:number;
+  service = new google.maps.DistanceMatrixService();
   ngOnInit() {
-
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+    // var myLatlng = new google.maps.LatLng(, );
+    var myLatlng = new google.maps.LatLng(this.lat, this.lng);
+    
     var mapOptions = {
         zoom: 13,
         center: myLatlng,
@@ -112,14 +143,31 @@ export class MapsComponent implements OnInit {
 
     };
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        title: "Hello World!"
-    });
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var curLat=position.coords.latitude;
+        var curLng=position.coords.longitude;
+        var curMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(curLat,curLng),
+            title: "Hello World!"
+        });
+        curMarker.setMap(map);
+      })
+    
 
     // To add the marker to the map, call setMap();
-    marker.setMap(map);
+    let numbers = [1, 2, 3];
+    
+    for (let num of numbers) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(this.lat, this.lng),
+            title: "Hello World!",
+            icon:"/assets/img/tomato.png"
+        });
+        this.lat-=0.5;
+        this.lng+=0.5;
+        marker.setMap(map);
+    }
+    
   }
 
 }
